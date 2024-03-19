4d013c3d2f93ee34c604ac72b3477d869a201c23
mustaaaaa <moustafaibrahim1512@gmail.com>
2024-03-19 16:56:25 +0100

    add: creati l'array listainvitati e nomiospiti

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// *SNACK 1*
+// Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+// Ci ha lasciato il nome del tavolo (“Tavolo Vip”) e la lista degli invitati in ordine di posto:
+
+// creo l'array listaInvitati.
+const listaInvitati = [
+    {
+        nomeTavolo: 'Tavolo Vip',
+        nomeOspite: [],
+        posto: []
+    }
+];
+// creo l'array nomiOspiti.
+const nomiOspiti = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
