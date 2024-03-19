279356f4db3d11a8ea40582b7e1f7f5664c091da
mustaaaaa <moustafaibrahim1512@gmail.com>
2024-03-19 18:26:39 +0100

    add: snack 2- aggiunto l'array e la lista nomi in maiscolo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+console.log('inizio snack 1');
 // *SNACK 1*
 // Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
 // Ci ha lasciato il nome del tavolo (“Tavolo Vip”) e la lista degli invitati in ordine di posto:
@@ -56,5 +57,66 @@ const listaSegnaPosto = nomiOspiti.map((elNome, i) => {
 });
 
 console.log(listaSegnaPosto);
+console.log('fine snack 1');
 
+
+console.log('snack 2');
+    // *SNACK 2*
+// Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+// 1. Per preparare l’aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+// ES (Marco della Rovere => MARCO DELLA ROVERE);
+// 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+// 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+// Questo è l’elenco degli studenti:
+// Id   Name                    Grades
+// 213  Marco della Rovere      78
+// 110  Paola Cortellessa       96
+// 250  Andrea Mantegna 	      48
+// 145  Gaia Borromini          74
+// 196  Luigi Grimaldello 	  68
+// 102  Piero della Francesca   50
+// 120  Francesca da Polenta    84
+
+
+const listaStudenti = [
+    {
+        id: 213,
+        nome: 'Marco della Rovere ',
+        voto: 78
+    },
+    {
+        id: 110,
+        nome: 'Paola Cortellessa',
+        voto: 96
+    },
+    {
+        id: 250,
+        nome: 'Andrea Mantegna',
+        voto: 48
+    },
+    {
+        id: 145,
+        nome: 'Gaia Borromini',
+        voto: 74
+    },
+    {
+        id: 196,
+        nome: 'Luigi Grimaldello',
+        voto: 68
+    },
+    {
+        id: 102,
+        nome: 'Piero della Francesca',
+        voto: 50
+    },
+    {
+        id: 120,
+        nome: 'Francesca da Polenta',
+        voto: 84
+    }
+];
+
+// lista nomi in maiuscolo
+const nomeInMaiuscolo = listaStudenti.map(studente => studente.nome.toUpperCase());
+console.log(nomeInMaiuscolo);
 
