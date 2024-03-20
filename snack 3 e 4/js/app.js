c676ab806976e48e52bbf2a58eeac82f51f30bf0
mustaaaaa <moustafaibrahim1512@gmail.com>
2024-03-20 14:46:03 +0100

    add: array snack 3

---
+++ snack 3 e 4/js/app.js
@@ -0,0 +1,27 @@
+// SNACK 3
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+const elencoBici = [
+    {
+        nomeBici: 'Mountain Crusher',
+        peso: 12
+    },
+    {
+        nomeBici: 'Speed Demon',
+        peso: 15
+    },
+    {
+        nomeBici: 'Urban Cruiser',
+        peso: 9
+    },
+    {
+        nomeBici: 'Stealth Rider',
+        peso: 18
+    },
+    {
+        nomeBici: 'Electric Thunder',
+        peso: 11
+    }
+];
